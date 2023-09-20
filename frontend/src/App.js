eb9ef18a31c12e97437043fbc291bc1b0a84863d
SivaSrinivasanS <145287408+SivaSrinivasanS@users.noreply.github.com>
2023-09-20 19:33:35 +0530

    Add files via upload

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ImageUpload from './home';
+
+function App() {
+  return (
+    <div className="App">
+      <ImageUpload />
+    </div>
+  );
+}
+
+export default App;
+
